65f6d3c140f6342785c3b927e313aa8ace370604
Berk Yücetaş <109201325+berkyucetas44@users.noreply.github.com>
2022-09-12 02:28:33 +0300

    Create odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,20 @@
+--INNER JOIN --
+
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--1.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) 
+--isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select city as şehir, country as ülke from city
+inner join country on city.country_id = country.country_id
+
+--2.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
+--first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+select payment.payment_id, customer.last_name as soyad, customer.first_name as isim from payment
+inner join customer on payment.customer_id = customer.customer_id
+
+ --3.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki 
+ --first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+select rental_id, first_name, last_name from rental
+inner join customer on rental.customer_id = customer.customer_id
